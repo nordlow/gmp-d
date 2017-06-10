98b65cfa726d49e9fe02a2ff60f651fda026c847
Per Nordlöw <per.nordlow@gmail.com>
2017-06-10 23:03:01 +0200

    Remove unused traits

--- src/gmp/traits.d
+++ src/gmp/traits.d
@@ -6,19 +6,10 @@ module gmp.traits;
 */
 enum isArithmetic(T) = __traits(isArithmetic, T);
 enum isFloating(T) = __traits(isFloating, T);
-enum isFloatingPoint(T) = __traits(isFloating, T);
-enum isScalar(T) = __traits(isScalar, T);
-enum isScalarType(T) = __traits(isScalar, T);
 enum isIntegral(T) = __traits(isIntegral, T);
 enum isUnsigned(T) = __traits(isUnsigned, T);
 enum isSigned(T) = __traits(isArithmetic, T) && !__traits(isUnsigned, T);
-enum isStaticArray(T) = __traits(isStaticArray, T);
-enum isAssociativeArray(T) = __traits(isAssociativeArray, T);
-enum isPOD(T) = __traits(isPOD, T);
-enum isNested(T) = __traits(isNested, T);
-enum isOut(alias fn) = __traits(isOut, fn);
 enum isLazy(alias fn) = __traits(isLazy, fn);
-enum isTemplate(alias sym) = __traits(isTemplate, sym);
 enum isRef(alias fn) = __traits(isRef, fn);
 
 /** Is `true` iff `T` is a GNU MP arithmetic type (`long`, `ulong` or `double`). */
