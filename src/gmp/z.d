3c53e79fcd307b6e8aa121204ca811766f2b7770
Per Nordlöw <per.nordlow@gmail.com>
2017-06-10 20:52:34 +0200

    More tests for toHash

--- src/gmp/z.d
+++ src/gmp/z.d
@@ -1469,6 +1469,7 @@ _MpZ!copyable powm(bool copyable)(auto ref const _MpZ!copyable base,
     assert(w.toHash == 0);
     w = 42;
     assert(w.toString == `42`);
+    assert(w.toHash != 0);      // should at least be non-zero
 }
 
 /// null construction
