d2828c6cf87131f47009c8477dc642f8cc6636ce
Ivan Baidakou <the.dmol@yandex.by>
2018-05-27 20:55:49 +0300

    Add mpz_export (#8)
    * Add mpz_export
    
    * use T[] instead of void*
    
    * Additional debug checks
    
    * Improvements
    
    * Add slice-allocating convert()
    
    * Handle @nogc
    
    * Better enum names & values
    
    * Feedback: more compact version

--- src/gmp/z.d
+++ src/gmp/z.d
@@ -15,6 +15,18 @@ enum isMpZExpr(T) = (is(Unqual!(typeof(T.eval())) == MpZ)); // which returns an 
 enum isLazyMpZExpr(T) = (!is(Unqual!T == MpZ) &&            // exclude direct value
                          isMpZExpr!T);
 
+enum Endianess {
+    host,
+    bigEndian,
+    littleEndian,
+};
+
+enum WordOrder {
+    mostSignificantWordFirst,
+    leastSignificantWordFirst,
+};
+
+
 // TODO use these imports instead of the ones below
 // import deimos.gmp.gmp;
 // import deimos.gmp.integer;
@@ -74,6 +86,21 @@ private struct _MpZ(bool copyable = false)
             hash ^= _limbs[i].hashOf;
         }
         return hash;
+    }
+
+    /** Allocates slice and exports to it arbitrary words of binary data into . It's format defined by:
+        - `order`: the most significant word `first` or `last` for least significant first
+        - `endian` can be `bigEndian`, `littleEndian` or `host` default
+        - the most significant `nails` bits of each word are unused and set to zero, this can be 0 to produce full words
+
+        Returns: the new slice with the number of words produced
+     */
+    pragma(inline, true)
+    T[] convert(T)(WordOrder order, Endianess endian, size_t nails) const @trusted
+    {
+        auto numb = 8 * T.sizeof - nails;
+        size_t count = (__gmpz_sizeinbase(_ptr, 2) + numb-1) / numb;
+        return convert(new T[count], order, T.sizeof, endian, nails);
     }
 
     @nogc:
@@ -1081,6 +1108,43 @@ private struct _MpZ(bool copyable = false)
         }
     }
 
+    /** Exports to arbitrary words of binary data into `rop` pre-allocated slice. It's format defined by:
+        - `order`: the most significant word `first` or `last` for least significant first
+        - `size` in bytes of each word
+        - `endian` can be `bigEndian`, `littleEndian` or `host` default
+        - the most significant `nails` bits of each word are unused and set to zero, this can be 0 to produce full words
+
+        Returns: the (sub-)slice with the number of words produced
+     */
+    pragma(inline, true)
+    T[] convert(T)(T[] rop, WordOrder order, size_t size, Endianess endian, size_t nails) const @trusted
+    {
+        assert(rop, "rop undefined");
+        size_t count;
+        debug {
+            auto numb = 8 * size - nails;
+            size_t items = (__gmpz_sizeinbase(_ptr, 2) + numb-1) / numb;
+            assert(T.sizeof * rop.length >= items * size , "rop has no enough space pre-allocated");
+        }
+
+        int realOrder;
+        final switch(order) {
+            case WordOrder.mostSignificantWordFirst:  realOrder = 1;  break;
+            case WordOrder.leastSignificantWordFirst: realOrder = -1; break;
+        }
+
+        int realEndian;
+        final switch(endian) {
+            case Endianess.littleEndian: realEndian = -1; break;
+            case Endianess.bigEndian:    realEndian =  1; break;
+            case Endianess.host:         realEndian =  0; break;
+        }
+
+        __gmpz_export(rop.ptr, &count, realOrder, size, realEndian, nails, _ptr);
+        return rop[0 .. count];
+    }
+
+
     /// Returns: `true` iff `this` fits in a `T`.
     pragma(inline, true)
     bool fitsIn(T)() const @trusted
@@ -1760,6 +1824,26 @@ _MpZ!copyable invert(bool copyable)(auto ref const _MpZ!copyable base,
     assert(mpz(-42).absUnsign!ulong == 42);
     assert(mpz( 42).absUnsign!ulong == 42);
 }
+
+///
+@nogc unittest
+{
+    ubyte[int.sizeof] storage;
+    ubyte[1] expected = [2];
+    assert(storage[0] == 0);
+    auto storage2 =  2.Z.convert(storage, WordOrder.mostSignificantWordFirst, 1, Endianess.littleEndian, 0);
+    assert(storage2.ptr == storage.ptr);
+    assert(storage2 == expected);
+}
+
+unittest
+{
+    ubyte[int.sizeof] storage;
+    ubyte[1] expected = [2];
+    ubyte[] storage2 = 2.Z.convert!(ubyte)(WordOrder.mostSignificantWordFirst, Endianess.littleEndian, 0);
+    assert(storage2 == expected);
+}
+
 
 /// opBinary with r-value right-hand-side
 @safe @nogc unittest
@@ -3059,6 +3143,7 @@ extern(C) pragma(inline, false)
 
     char *__gmpz_get_str (char*, int, mpz_srcptr);
     size_t __gmpz_sizeinbase (mpz_srcptr, int);
+    void *__gmpz_export (void* , size_t*, int, size_t, int, size_t, mpz_srcptr);
 
     void __gmpz_powm (mpz_ptr, mpz_srcptr, mpz_srcptr, mpz_srcptr);
     void __gmpz_powm_ui (mpz_ptr, mpz_srcptr, ulong, mpz_srcptr);
